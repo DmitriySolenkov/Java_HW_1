d79c27fcb434c1a6c38448cc7e841ac23aa55cea
KreoX <soldimksol@gmail.com>
2022-11-27 14:40:28 +0300

    Ex2Format

--- Ex2.java
+++ Ex2.java
@@ -1,18 +1,17 @@
 public class Ex2 {
     public static void main(String[] args) {
-        int num=1;
+        int num = 1;
         System.out.format("%d ", num);
         for (num = 2; num <= 1000; num++) {
             int index = 2;
             while (index <= num) {
                 if (index == num) {
                     System.out.format("%d ", num);
-                    index=1;
+                    index = 1;
                 }
                 if (num % index == 0) {
                     break;
-                }
-                else{
+                } else {
                     index++;
                 }
 
